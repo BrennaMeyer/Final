072209f2a349a64da40a7958a9492ef21b9c4663
Brenna Meyer <brennameyer9@gmail.com>
2025-05-06 13:14:39 -0500

    Add project files.

---
+++ Final.cpp
@@ -0,0 +1,29 @@
+// Final.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer: Brenna Meyer
+// Date:05/06/2025
+//Requirements: Write a modular program that asks the user for a file name. Assume the file contains a series of numbers, each written on a separate line. The program should read the contents of the file into an array then display the following data:
+//• The lowest number in the array
+//• The highest number in the array
+//• The total of the numbers in the array
+//• The average of the numbers in the array
+//To test the program, you can download the file numbers.txt or ListOfNumbers.txt
+//You may also want to create your own file with something funky in it to see how
+//well your code handles it.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
